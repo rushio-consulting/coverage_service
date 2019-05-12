ceea3cc02dbf71b4b42c010f1eb0ffccfb7320bd
bwnyasse <risbonyasse@gmail.com>
2019-05-12 23:51:09 +0200

    Update ID option value

--- main.sh
+++ main.sh
@@ -3,12 +3,15 @@
 # @description : 
 #
 
-ID=123456789
+#TODO: Generated UUID 
+UUID=$(uuidgen)
+ID=${UUID,,} # lower case
+
 EXPECTED_OUTPUT_COVERAGE_DIR="/tmp/rushio-gen-coverage-$ID/coverage"
 
 dart bin/main.dart \
     -p /coverage-project \
-    -i 123456789
+    -i $ID
 
 #TODO: Check if last command success 
 
